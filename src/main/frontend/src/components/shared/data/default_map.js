c47585998d79ee47ec27fe1f1b4a4afa0c415683
samdaseuss <ohj3423@naver.com>
2024-06-16 19:28:55 +0900

    remove: 파일 삭제

--- src/main/frontend/src/components/shared/data/default_map.js
+++
@@ -1,6 +0,0 @@
-export const center = {
-  lat: 37.566826,
-  lng: 127.1,
-}
-
-export const level = 8.8
--- src/main/frontend/src/components/shared/data/outerRectangle.js
+++
@@ -1,6 +0,0 @@
-export const outerRectanglePath = [
-  {lat: 38.5, lng: 126.0},
-  {lat: 38.5, lng: 128.0},
-  {lat: 35.0, lng: 128.0},
-  {lat: 35.0, lng: 126.0},
-]
