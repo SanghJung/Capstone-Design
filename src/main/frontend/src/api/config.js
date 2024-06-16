cb6dd5b30fb1d487850aad1025733b5e2b0fd98a
samdaseuss <ohj3423@naver.com>
2024-06-16 23:53:21 +0900

    feat: 타입 정의 * 백엔드 url * 전체 지역 경계 값 * 색상 * 지도 초기값 etc. Related to: #28, #30, #44, #45, #46

--- src/main/frontend/src/api/config.js
+++ src/main/frontend/src/api/config.js
@@ -1,3 +1,10 @@
-const BACKEND_URL = 'http://localhost:8080'
+const BACKEND_SERVER = 'http://localhost:8080/api'
 
-export default BACKEND_URL
+const API_URLS = {
+  GET_ALL_PLACE: `http://localhost:8080/api/places`,
+  GET_STORE_INFO: `http://localhost:8080/api/info`,
+  CHOSEN_PLACE: (place_id) => `${BACKEND_SERVER}/chosen/${place_id}`,
+  UNCHOSEN_PLACE: `http://localhost:8080/api/unchosen`,
+}
+
+export default API_URLS
---
+++ src/main/frontend/src/data/default_map.js
@@ -0,0 +1,12 @@
+export const center = {
+  lat: 37.566826,
+  lng: 127.1,
+}
+
+export const gangnamArea = {
+  area: 38731528,
+  name: '강남구',
+  position: {lat: 37.51507332009245, lng: 127.04452102475092},
+}
+
+export const level = 8.6
